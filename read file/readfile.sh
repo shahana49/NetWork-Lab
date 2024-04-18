a2d4ac5a8ddb6fadd33355f605caade594496a6b
shahana49 <145976313+shahana49@users.noreply.github.com>
2024-04-18 16:07:10 +0530

    Add files via upload

---
+++ read file/readfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter filename"
+read file
+c=`cat $file | wc -c`
+w=`cat $file | wc -w`
+l=`grep -c "." $file`
+echo "No of charectors in $file is $c"
+echo "No of words in $file is $w"
+echo "No of lines in $file is $l"
